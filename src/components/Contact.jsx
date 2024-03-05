62a7f9ce3bc7ee446052fe1498cd9c97381dd3e5
wanhasibala <wanhasibal1122@gmail.com>
2024-03-05 21:53:40 +0700

    fix(contact): remove after submitting email from client

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,24 @@
 import emailjs from "@emailjs/browser";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Contact() {
+  const [formData, setFormData] = useState({ first_name :'', last_name: '' ,user_email: '', subject:'', message:'' });
   const form = useRef();
+  const handleChange= (e) =>{
+    setFormData({
+      ...formData,
+      [e.target.name] :e.target.value
+    })
+  }
   const sendEmail = (e) => {
     e.preventDefault();
+    setFormData({
+      first_name: '',
+      last_name: '',
+      user_email: '',
+      subject: '',
+      message: ''
+    })
 
     emailjs
       .sendForm("service_z98kpf6", "template_jwhxnpk", form.current, {
@@ -33,28 +47,38 @@ export default function Contact() {
               placeholder=" First Name"
               type="text"
               name="first_name"
+              value={formData.first_name}
+              onChange={handleChange}
             />
             <label htmlFor="Name" />
             <input
               className="border-b border-secondary-300 w-full py-1 bg-transparent "
               placeholder="Last Name"
               name="last_name"
+              value={formData.last_name}
+              onChange={handleChange}
             />
           </div>
           <input
             className="border-b border-secondary-300 w-full py-1 bg-transparent mb-10"
             placeholder="Email"
             name="user_email"
+            value={formData.user_email}
+            onChange={handleChange}
           />
           <input
             className="border-b border-secondary-300 w-full py-1 bg-transparent mb-10"
             placeholder="Subject"
             name="subject"
+            value={formData.subject}
+              onChange={handleChange}
           />
           <textarea
             className="border-b border-secondary-300 w-full py-1 bg-transparent mb-10 pb-20 resize-none overflow-hidden"
             name="message"
             placeholder="Message"
+            value={formData.message}
+              onChange={handleChange}
           />
           <div className="w-full flex items-center justify-center">
 
